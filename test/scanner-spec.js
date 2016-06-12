eadb14b4085910a2fb76a51a8a3e5293c5804dd5
Cory Roloff <cory@brothersroloff.com>
2016-06-12 09:51:19 -0500

    Add initial project

---
+++ test/scanner-spec.js
@@ -0,0 +1,85 @@
+import {expect} from "chai";
+import Scanner from "../lib/scanner";
+
+describe("scanner", function () {
+	let scanner;
+
+	beforeEach(function () {
+		scanner = new Scanner("12345");
+	});
+
+	context("constructor", function () {
+		it("should set code as an array of characters from the original string", function () {
+			expect(scanner.code).to.deep.equal(["1", "2", "3", "4", "5"]);
+		});
+
+		it("should start index at 0", function () {
+			expect(scanner.index).to.equal(0);
+		});
+
+		it("should set lineNumber to 0 if an empty string is given", function () {
+			expect(new Scanner("").lineNumber).to.equal(0);
+		});
+
+		it("should default lineStart to 1", function () {
+			expect(scanner.lineStart).to.equal(1);
+		});
+	});
+
+	context("isLineTerminator", function () {
+		for (let char of ["\n", "\r", "\r\n", "\u2028", "\u2029"]) {
+			it(`should return true for: ${char}`, function () {
+				expect(scanner.isLineTerminator(char)).to.be.true;
+			});
+		}
+
+		it("should return false if not a line ending", function () {
+			expect(scanner.isLineTerminator("A")).to.be.false;
+		});
+	});
+
+	context("read", function () {
+		it("should read each character, move the cursor, and return undefined at the end", function () {
+			expect(scanner.read()).to.equal("1");
+			expect(scanner.index).to.equal(1);
+			expect(scanner.read()).to.equal("2");
+			expect(scanner.index).to.equal(2);
+			expect(scanner.read()).to.equal("3");
+			expect(scanner.index).to.equal(3);
+			expect(scanner.read()).to.equal("4");
+			expect(scanner.index).to.equal(4);
+			expect(scanner.read()).to.equal("5");
+			expect(scanner.index).to.equal(5);
+			expect(scanner.read()).to.be.undefined;
+			expect(scanner.index).to.equal(6);
+		});
+
+		for (let char of ["\n", "\r", "\r\n", "\u2028", "\u2029"]) {
+			it(`should increment the line number and line start when it hits a new line: ${char}`, function () {
+				let scanner = new Scanner(`ab${char}c`);
+				scanner.read();
+				expect(scanner.lineNumber).to.equal(1);
+				expect(scanner.lineStart).to.equal(1);
+				scanner.read();
+				scanner.read();
+				expect(scanner.lineNumber).to.equal(2);
+				expect(scanner.lineStart).to.equal(3);
+				expect(scanner.index).to.equal(3);
+			});
+		}
+	});
+
+	context("peek", function () {
+		it("should return the next available character without moving the cursor", function () {
+			expect(scanner.peek()).to.equal("1");
+			expect(scanner.index).to.equal(0);
+			expect(scanner.char).to.be.null;
+		});
+
+		it("should return undefined if at the end of the string", function () {
+			let scanner = new Scanner("a");
+			scanner.read();
+			expect(scanner.peek()).to.be.undefined;
+		});
+	});
+});
---
+++ test/tokenizer-spec.js
@@ -0,0 +1,71 @@
+import {expect} from "chai";
+import tokenize from "../lib/tokenize";
+import alphabet from "alphabet";
+
+describe("tokenizer", function () {
+	for (let char of alphabet.concat(["_", "$"])) {
+		it(`should tokenize when character has prefix: ${char}`, function () {
+			expect(tokenize(`{${char}char}`)).to.deep.equal([
+				{type: "Punctuator", value: "{", loc: {start: {line: 1, column: 1}, end: {line: 1, column: 2}}, range: {start: 1, end: 2}},
+                {type: "Identifier", value: `${char}char`, loc: {start: {line: 1, column: 2}, end: {line: 1, column: 7}}, range: {start: 2, end: 7}},
+				{type: "Punctuator", value: "}", loc: {start: {line: 1, column: 7}, end: {line: 1, column: 8}}, range: {start: 7, end: 8}}
+			]);
+		});
+	}
+
+	for (let char of ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) {
+		it(`should not tokenize when character has prefix: ${char}`, function () {
+			expect(() => tokenize(`{${char}char}`)).to.throw();
+		});
+	}
+
+	it("should parse paths of identifiers and punctuators (.)", function () {
+		expect(tokenize("{one.two.three}")).to.deep.equal([
+			{type: "Punctuator", value: "{", loc: {start: {line: 1, column: 1}, end: {line: 1, column: 2}}, range: {start: 1, end: 2}},
+			{type: "Identifier", value: "one", loc: {start: {line: 1, column: 2}, end: {line: 1, column: 5}}, range: {start: 2, end: 5}},
+			{type: "Punctuator", value: ".", loc: {start: {line: 1, column: 5}, end: {line: 1, column: 6}}, range: {start: 5, end: 6}},
+			{type: "Identifier", value: "two", loc: {start: {line: 1, column: 6}, end: {line: 1, column: 9}}, range: {start: 6, end: 9}},
+			{type: "Punctuator", value: ".", loc: {start: {line: 1, column: 9}, end: {line: 1, column: 10}}, range: {start: 9, end: 10}},
+			{type: "Identifier", value: "three", loc: {start: {line: 1, column: 10}, end: {line: 1, column: 15}}, range: {start: 10, end: 15}},
+			{type: "Punctuator", value: "}", loc: {start: {line: 1, column: 15}, end: {line: 1, column: 16}}, range: {start: 15, end: 16}}
+		]);
+	});
+
+	it("should tokenize comments with quotes and other syntax", function () {
+		expect(tokenize("{! hello \" {one} {#hey}{/hey} !}")).to.deep.equal([
+			{type: "Comment", value: " hello \" {one} {#hey}{/hey} ", loc: {start: {line: 1, column: 1}, end: {line: 1, column: 34}}, range: {start: 1, end: 34}}
+		]);
+	});
+
+	it("should tokenize literals with escaped double quotes inside of them", function () {
+		expect(tokenize("{\"lorem \\\" ipsum\"}")).to.deep.equal([
+			{type: "Punctuator", value: "{", loc: {start: {line: 1, column: 1}, end: {line: 1, column: 2}}, range: {start: 1, end: 2}},
+			{type: "Literal", value: "\"lorem \\\" ipsum\"", loc: {start: {line: 1, column: 2}, end: {line: 1, column: 18}}, range: {start: 2, end: 18}},
+			{type: "Punctuator", value: "}", loc: {start: {line: 1, column: 18}, end: {line: 1, column: 19}}, range: {start: 18, end: 19}}
+		]);
+	});
+
+	it("should tokenize non-syntax characters as buffers", function () {
+		expect(tokenize("<h1>{hello},{world}!</h1>")).to.deep.equal([
+			{type: "Buffer", value: "<h1>", loc: {start: {line: 1, column: 1}, end: {line: 1, column: 5}}, range: {start: 1, end: 5}},
+			{type: "Punctuator", value: "{", loc: {start: {line: 1, column: 5}, end: {line: 1, column: 6}}, range: {start: 5, end: 6}},
+			{type: "Identifier", value: "hello", loc: {start: {line: 1, column: 6}, end: {line: 1, column: 11}}, range: {start: 6, end: 11}},
+			{type: "Punctuator", value: "}", loc: {start: {line: 1, column: 11}, end: {line: 1, column: 12}}, range: {start: 11, end: 12}},
+			{type: "Buffer", value: ",", loc: {start: {line: 1, column: 12}, end: {line: 1, column: 13}}, range: {start: 12, end: 13}},
+			{type: "Punctuator", value: "{", loc: {start: {line: 1, column: 13}, end: {line: 1, column: 14}}, range: {start: 13, end: 14}},
+			{type: "Identifier", value: "world", loc: {start: {line: 1, column: 14}, end: {line: 1, column: 19}}, range: {start: 14, end: 19}},
+			{type: "Punctuator", value: "}", loc: {start: {line: 1, column: 19}, end: {line: 1, column: 20}}, range: {start: 19, end: 20}},
+			{type: "Buffer", value: "!</h1>", loc: {start: {line: 1, column: 20}, end: {line: 1, column: 26}}, range: {start: 20, end: 26}}
+		]);
+	});
+
+	it("should tokenize whitespace inside of syntax", function () {
+		expect(tokenize("{ \t\v\f\u00A0\uFEFFa }")).to.deep.equal([
+			{type: "Punctuator", value: "{", loc: {start: {line: 1, column: 1}, end: {line: 1, column: 2}}, range: {start: 1, end: 2}},
+			{type: "Whitespace", value: " \t\v\f\u00A0\uFEFF", loc: {start: {line: 1, column: 2}, end: {line: 1, column: 8}}, range: {start: 2, end: 8}},
+			{type: "Identifier", value: "a", loc: {start: {line: 1, column: 8}, end: {line: 1, column: 9}}, range: {start: 8, end: 9}},
+			{type: "Whitespace", value: " ", loc: {start: {line: 1, column: 9}, end: {line: 1, column: 10}}, range: {start: 9, end: 10}},
+			{type: "Punctuator", value: "}", loc: {start: {line: 1, column: 10}, end: {line: 1, column: 11}}, range: {start: 10, end: 11}}
+		]);
+	});
+});
